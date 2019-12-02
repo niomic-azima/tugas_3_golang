eb1bffc6f147e2b5f02203966e417ac570e3daca
Azima-niomic <hades2317@gmail.com>
2019-12-03 03:18:59 +0800

    Tugas 3 Golang
    Tugas niomic

---
+++ tugas3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var buah = []string{"apel", "jeruk", "anggur", "melon"}
+	buah = append(buah, "pepaya")
+
+	fmt.Println("Jumlah Element :", len(buah))
+	fmt.Println("Isi Element :", buah)
+	for i, b := range buah {
+		fmt.Println("Element ke - :", i, b)
+	}
+}
